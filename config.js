b4decec11ef86ee0ecb01afb5e6584aba9beb69f
Brenda Hensley <renaeehensley@gmail.com>
2025-03-25 20:25:05 -0400

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// config.js
-const CONFIG = {
-    EMAILJS_PUBLIC_KEY: "ZGbpA7CJbdnOBQD3j",
-    EMAILJS_SERVICE_ID: "service_3vkawg5",
-    EMAILJS_TEMPLATE_ID: "template_3bmnj6a",
-  };
-  
-  export default CONFIG;
